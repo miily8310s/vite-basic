3f06af69078013ed420ba6e42f42d5f178de0874
miily8310s <rillakerika@gmail.com>
2021-07-11 10:56:26 +0900

    Finish React App

--- react-basic-vite/src/components/Dogs.tsx
+++ react-basic-vite/src/components/Dogs.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+const RANDOM_DOGS_URL = 'https://dog.ceo/api/breeds/image/random';
 
 const Dogs = (): JSX.Element => {
+  
+  const [dogImage, setDogImage] = useState('');
+  const [dogBreed, setDogBreed] = useState('');
+  const getBreed = (imageName: string) => {
+    // https://images.dog.ceo/breeds/pug/xxxxx.jpg
+    return imageName.match(/(?<=breeds\/)\w+/);
+  }
+  
+  useEffect(() => {
+    return () => {
+      axios
+        .get(RANDOM_DOGS_URL)
+        .then((res) => {
+          const {data} = res
+          setDogImage(data.message)
+          if (getBreed(dogImage) !== null) {
+            setDogBreed(getBreed(dogImage));
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  }, []);
+
   return (
     <main>
       <div className='dog'>
-        <img src='#' alt='#' />
-        <p>dog dog dog</p>
+        <img src={dogImage} alt={dogBreed} />
+        <p>{dogBreed || 'unknown dog breed' }</p>
       </div>
     </main>
   );
