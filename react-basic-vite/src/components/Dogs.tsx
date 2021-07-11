34936978e763436c654fa009adad7207ddaa7cbd
miily8310s <rillakerika@gmail.com>
2021-07-11 09:53:19 +0900

    Add Dogs component to React App

---
+++ react-basic-vite/src/components/Dogs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dogs = (): JSX.Element => {
+  return (
+    <main>
+      <div className='dog'>
+        <img src='#' alt='#' />
+        <p>dog dog dog</p>
+      </div>
+    </main>
+  );
+};
+
+export default Dogs;
